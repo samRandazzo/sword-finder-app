064c8b90998bee000462036ae00b65952bc37c45
sam <sam.randazzo@protonmail.com>
2019-07-25 14:49:22 -0400

    inventory component

--- workspace/swordFinder/src/app/app.module.ts
+++ workspace/swordFinder/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
